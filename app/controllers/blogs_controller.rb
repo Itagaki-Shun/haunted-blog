39071e758b0e506ca2fb1c5bbe0e98897d82a437
itagaki <itagaki_shun@vitallead.co.jp>
2025-08-07 09:39:34 +0900

    check_secretメソッドを削除してset_blogに統合
    [目的]
    - 秘密投稿チェックロジックを統合し、コードの可読性と保守性を向上させる
    
    [変更内容]
    - check_secretメソッドを削除
    - before_action :check_secretを削除
    - set_blogメソッドのelse節でWHERE条件を追加
    - SQLレベルで公開ブログまたは自分のブログのみ取得するよう変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,6 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
-  before_action :check_secret, only: %i[show]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -48,15 +47,13 @@ class BlogsController < ApplicationController
     @blog = if action_name.in?(%w[edit update destroy])
               current_user.blogs.find(params[:id])
             else
-              Blog.find(params[:id])
+              Blog
+                .where('id = ? AND (secret = false OR user_id = ?)', params[:id], current_user)
+                .first!
             end
   end
 
   def blog_params
     params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
-  end
-
-  def check_secret
-    raise ActiveRecord::RecordNotFound if @blog.secret? && @blog.user != current_user
   end
 end
